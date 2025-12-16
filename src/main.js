88d75ca3191778012784357fdae24770d650292d
Jayeon Hwang <jayeon512@gmail.com>
2025-12-16 10:01:16 -0600

    add next level button

--- src/main.js
+++ src/main.js
@@ -5,37 +5,42 @@ import { GameBuilder, Reason } from "./game.js";
 import * as sound from './sound.js';
 
 
+let itemCount = 5;
+
 const gameFinishBanner = new PopUp();
 const game = new GameBuilder()
-  .withGameDuration(5)
-  .withCarrotCount(5)
-  .withBugCount(5)
+  .withGameDuration(10)
+  .withCarrotCount(itemCount)
+  .withBugCount(itemCount)
   .build();
 
 
 game.setGameStopListener(reason => {
-  let message;
   switch (reason) {
     case Reason.cancel:
-      message = 'Replay ❓';
       sound.playAlert();
       break;
     case Reason.win:
-      message = 'You win 🎉';
       sound.playWin();
       break;
     case Reason.lose:
-      message = 'You lose 😭';
       sound.playBug();
       break;
     default:
       throw new Error('not valid reason');
   }
-  gameFinishBanner.showWithText(message);
+  gameFinishBanner.show(reason);
 });
 
+
 gameFinishBanner.setClickListener(() => {
-  game.start();
+  if (Reason.win) {
+    itemCount++;
+    game.start();
+  } else {
+    itemCount = 5;
+    game.start();
+  }
 });
 
 
--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,20 @@
 'use strict';
+import { Reason } from "./game.js";
 export default class PopUp {
   constructor() {
-    this.PopUp = document.querySelector(".pop-up");
+    this.popUp = document.querySelector(".pop-up");
     this.popUpText = document.querySelector(".pop-up__message");
+    this.popUpNextLevel = document.querySelector(".pop-up__next_level");
     this.popUpRefresh = document.querySelector(".pop-up__refresh");
     this.popUpRefresh.addEventListener('click', () => {
       this.onClick && this.onClick();
       this.hide();
     });
+    this.popUpNextLevel.addEventListener('click', () => {
+      this.onClick && this.onClick();
+      this.hide();
+    });
+
   }
 
   setClickListener(onClick) {
@@ -15,12 +22,35 @@ export default class PopUp {
   }
 
   hide() {
-    this.PopUp.classList.add('pop-up--hide');
+    this.popUp.classList.add('pop-up--hide');
   }
 
-  showWithText(text) {
-    this.popUpText.innerText = text;
-    this.PopUp.classList.remove('pop-up--hide');
+  show(reason) {
+
+    this.popUp.classList.remove('pop-up--hide');
+    this.popUpRefresh.classList.add('pop-up-btn--hide');
+    this.popUpNextLevel.classList.add('pop-up-btn--hide');
+
+    switch (reason) {
+      case Reason.win:
+        this.popUpText.textContent = 'YOU WIN 🎉';
+        this.popUpNextLevel.classList.remove('pop-up-btn--hide');
+
+        break;
+
+      case Reason.lose:
+        this.popUpText.textContent = 'YOU LOSE 😭';
+        this.popUpRefresh.classList.remove('pop-up-btn--hide');
+        this.popUpRefresh.onclick = () => this.onRetry();
+        break;
+
+      case Reason.cancel:
+        this.popUpText.textContent = 'Replay ❓';
+        this.popUpRefresh.classList.remove('pop-up-btn--hide');
+        this.popUpRefresh.onclick = () => this.onRetry();
+        break;
+
+    }
   }
 
 }
